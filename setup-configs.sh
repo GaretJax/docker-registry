c534b29aea5cfe9e394c41c4b12c6ba94554e3f4
Jonathan Stoppani <jonathan@stoppani.name>
2013-12-29 20:47:06 +0100

    Get storage engine from environment.

--- setup-configs.sh
+++ setup-configs.sh
@@ -7,13 +7,25 @@ if [[ -z "$GUNICORN_WORKERS" ]] ; then
     GUNICORN_WORKERS=4
 fi
 
+if [[ -z "$STORAGE_ENGINE" ]] ; then
+    STORAGE_ENGINE="s3"
+fi
+
 if [ "$SETTINGS_FLAVOR" = "prod" ] ; then
     config=$(<config/config.yml);
     config=${config//boto_bucket: REPLACEME/boto_bucket: $S3_BUCKET};
-    config=${config//s3_access_key: REPLACEME/s3_access_key: $AWS_ACCESS_KEY_ID};
-    config=${config//s3_secret_key: REPLACEME/s3_secret_key: $AWS_SECRET_KEY};
-    config=${config//s3_bucket: REPLACEME/s3_bucket: $S3_BUCKET};
-    config=${config//s3_encrypt: REPLACEME/s3_encrypt: ${S3_ENCRYPT:-False}};
-    config=${config//s3_secure: REPLACEME/s3_secure: ${S3_SECURE:-False}};
+    config=${config//storage: REPLACEME/storage: $STORAGE_ENGINE};
+    
+    if [ "$STORAGE_ENGINE" = "s3" ] ; then
+        config=${config//s3_access_key: REPLACEME/s3_access_key: $AWS_ACCESS_KEY_ID};
+        config=${config//s3_secret_key: REPLACEME/s3_secret_key: $AWS_SECRET_KEY};
+        config=${config//s3_bucket: REPLACEME/s3_bucket: $S3_BUCKET};
+        config=${config//s3_encrypt: REPLACEME/s3_encrypt: ${S3_ENCRYPT:-False}};
+        config=${config//s3_secure: REPLACEME/s3_secure: ${S3_SECURE:-False}};
+    else if [ "$STORAGE_ENGINE" = "gs" ] ; then
+        config=${config//gs_access_key: REPLACEME/gs_access_key: $GS_ACCESS_KEY_ID};
+        config=${config//gs_secret_key: REPLACEME/gs_secret_key: $GS_SECRET_KEY};
+        config=${config//gs_secure: REPLACEME/gs_secure: ${GS_SECURE:-False}};
+    fi
     printf '%s\n' "$config" >config/config.yml
 fi
